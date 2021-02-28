214916ea2d403238673bb37494b219697fdd507d
Sergey Smirnov <sergei.smirnov@gmail.com>
2021-02-28 15:28:48 +0200

    Change in hooks

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo "RUN CLANG-FORMAT"
-./scripts/pre-commit-format.sh
+./scripts/pre-push-format.sh
 
 echo "CHECK UNIT-TESTS"
 ./scripts/run-tests.sh
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- echo "!!!!!TESTS MUST PASS BEFORE COMMIT!!!!!"
+ echo "!!!!!TESTS MUST PASS BEFORE GIT PUSH!!!!!"
  exit 1
 fi
--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -6,7 +6,7 @@ cd "${0%/*}/.."
 CURR_DIR=$(pwd)
 
 echo "Installing GIT hooks..."
-# this command creates symlink to our pre-commit script
-ln -s $CURR_DIR/scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
+# this command creates symlink to pre-push script
+ln -s $CURR_DIR/scripts/pre-push.sh $GIT_DIR/hooks/pre-push
 
 
